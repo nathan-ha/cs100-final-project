4fffaa71eb309dac4b225bcc3b6be096c2d0af2f
kliang193 <liangkent@yahoo.com>
2024-03-12 01:50:36 -0700

    theif balancing

--- src/characters/thief.cpp
+++ src/characters/thief.cpp
@@ -10,13 +10,13 @@ Thief::Thief(const string &name, const string &race, int health, const Weapon &c
 
 void Thief::stab(Character &target)
 {
-    int target_new_health = target.get_health() - current_weapon.damage;
+    int target_new_health = target.get_health() - current_weapon.damage - current_weapon.damage;
     target.set_health(target_new_health);
 }
 
 void Thief::throw_knife(Character &target)
 {
-    int dmg_amount = current_weapon.damage * 2;
+    int dmg_amount = current_weapon.damage * 3;
     int target_new_health = target.get_health() - dmg_amount;
     target.set_health(target_new_health);
     throwing_knife_count--;
